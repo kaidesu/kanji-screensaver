ccc8f63a9d526370eda74d107f7375e1622d4284
Shea Lewis (Kai) <355659+kaidesu@users.noreply.github.com>
2022-02-21 23:37:04 -0800

    Set random color

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,33 @@
 import { useState, useEffect } from 'react'
 import kanji from './kanji.json'
 
+const colors = [
+    {
+        "bg": "from-red-700 to-red-500",
+        "fg": "text-red-200"
+    },
+    {
+        "bg": "from-sky-700 to-sky-500",
+        "fg": "text-sky-200"
+    },
+    {
+        "bg": "from-violet-700 to-violet-500",
+        "fg": "text-violet-200"
+    },
+    {
+        "bg": "from-gray-700 to-gray-500",
+        "fg": "text-gray-200"
+    },
+    {
+        "bg": "from-emerald-700 to-emerald-500",
+        "fg": "text-emerald-200"
+    }
+]
+
 export default function Home() {
     const [seconds, setSeconds] = useState(10)
     const [kanji, setKanji] = useState(getRandomKanji())
+    const [color, setColor] = useState(getRandomColor())
 
     const tick = () => {
         if (seconds === 0) {
@@ -21,20 +45,20 @@ export default function Home() {
     })
 
     return (
-        <div class="h-screen bg-gradient-to-br from-red-700 to-red-500 flex flex-col items-center justify-center">
-            <div class="text-14xl font-bold text-red-200">
+        <div className={`h-screen bg-gradient-to-br ${color.bg} flex flex-col items-center justify-center`}>
+            <div className={`text-14xl font-bold ${color.fg}`}>
                 {kanji.japanese}
             </div>
 
-            <div class="text-4xl font-bold text-red-200">
+            <div className={`text-4xl font-bold ${color.fg}`}>
                 {kanji.reading}
             </div>
 
-            <div class="text-4xl font-bold text-red-200 mt-4">
+            <div className={`text-4xl font-bold ${color.fg} mt-4`}>
                 {kanji.meaning}
             </div>
 
-            <div class="text-red-200 mt-4">
+            <div className={`${color.fg} mt-4`}>
                 {`${seconds.toString().padStart(2, '0')}`}
             </div>
         </div>
@@ -47,4 +71,12 @@ function getRandomKanji() {
     const randomKey = keys[randomIndex]
 
     return kanji[randomKey]
+}
+
+function getRandomColor() {
+    const keys = Object.keys(colors)
+    const randomIndex = Math.floor(Math.random() * keys.length)
+    const randomKey = keys[randomIndex]
+
+    return colors[randomKey]
 }
